a848d4bca1d50cbf4dddc38a1b209feb938b40c8
ernanyJ <julioernany@gmail.com>
2023-10-09 21:56:20 -0300

    Full implemented RegisterNewQuestions class

--- src/Program/Application.java
+++ src/Program/Application.java
@@ -1,11 +1,15 @@
 package Program;
 
 import services.FormsReader;
+import services.RegisterNewQuestions;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("""
                                 
@@ -23,10 +27,20 @@ public class Application {
                                 
                 """);
 
-        System.out.println(FormsReader.read());
+        int opt = sc.nextInt();
 
+        switch(opt){
+            case 1:
+                break;
+            case 2:
+                FormsReader.read();
+                break;
+            case 3:
+                RegisterNewQuestions.register();
+                break;
+        }
 
-
+        RegisterNewQuestions.register();
 
 
     }
--- src/services/RegisterNewQuestions.java
+++ src/services/RegisterNewQuestions.java
@@ -3,38 +3,51 @@ package services;
 import exceptionshandling.CrudException;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class RegisterNewQuestions {
     static final String FILE_PATH = "src/resources/forms.txt";
 
-    public RegisterNewQuestions() {
+    public static void register() {
         try {
             Scanner sc = new Scanner(System.in);
 
             System.out.println("Digite a sua pergunta abaixo: \n");
             String question = sc.nextLine();
 
-
-            if(question != null){
-                BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH));
+            if (question != null) {
                 BufferedReader br = new BufferedReader(new FileReader(FILE_PATH));
 
-                String lastLine, line;
-                while((line = br.readLine()) != null){
+                String lastLine = "", line;
+
+                while ((line = br.readLine()) != null) {
                     lastLine = line;
                 }
 
-                //TODO finish register new questions method
-                // lastLine.split()
+                String[] splited = splitLine(lastLine);
+                int nextNumber = (parseInt(splited[0])) + 1;
+                System.out.println(nextNumber);
 
-            } else {
-                throw new CrudException("Vocẽ não pode inserir um valor nulo como input.");
-            }
+                BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, true));
 
-        } catch(IOException e){
+                String str = nextNumber + " - " + question;
+                bw.newLine();
+                bw.write(str);
+                bw.close();
+
+            } else
+                throw new CrudException("Vocẽ não pode inserir um valor nulo.");
+
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static String[] splitLine(String lastLine) {
+        return lastLine.split(" -");
     }
 
 }
